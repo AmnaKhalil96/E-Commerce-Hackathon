b4bd3f6c89ae1ad8a75eca842493d8663be52bd5
AmnaKhalil96 <anna963621@gmail.com>
2024-12-09 18:30:48 +0500

    push code

---
+++ src/app/page.tsx
@@ -0,0 +1,42 @@
+import BEST from "./components/BESTAIR";
+import Featured from "./components/FEATURED";
+import Hero from "./components/hero";
+import { GearUpShop } from '@/components/GearUpShop'
+import Last from "./components/last";
+import Essential from "./components/essential";
+import Miss from "./components/miss";
+import Sidebar from "./components/allproducts";
+import Prodetail from "./components/prodetail";
+import LoginForm from "./components/loginform";
+
+import Cart from "./components/cart";
+import Order from "./components/order";
+import SignUpForm from "./components/Signup";
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <Hero/>
+      <BEST/>
+      <Featured/>
+      <GearUpShop />
+      <Miss/>
+      <Essential/>
+      <Last/>
+
+      <Sidebar/>
+
+      <Prodetail/>
+
+      <LoginForm/>
+
+      <SignUpForm/>
+
+      <Cart/>
+
+      <Order/>
+    </div>
+  );
+}
